5734fc763a12c066532ae84cb767fd5078ee0258
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-24 01:59:46 +0300

    Added login and register forms

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Submit from '../components/Submit'
+import Input from '../components/Input'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Auth(props) {
     const navigate = useNavigate()
+    const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleLogin = () => {
+    const handleLogin = async () => {
         try {
             let res = await axios({
-                url: "http://127.0.0.1:8000/api/login",
+                url: "http://127.0.0.1:777/login",
                 method: "POST",
                 data: {
-                    "email": email,
+                    "username": username,
                     "password": password
                 },
             })
@@ -20,11 +26,37 @@ export default function Auth(props) {
         catch (e) {
             console.log(e);
         }
-
-        return (
-            <>
-
-            </>
-        )
     }
+
+    return (
+        <>
+            <div>
+                <div>
+                    Survey Champ
+                </div>
+                <form>
+                    <div>
+                        <Input type={'username'} name={'username'} placeholder={'Username'} value={username} setValue={setUsername} />
+                        <Input type={'password'} name={'password'} placeholder={'Password'} value={password} setValue={setPassword} />
+                        <Submit value={'Login'} run={handleLogin} />
+                        <div>Dont have an accout? <span>Register</span></div>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <div>
+                    Survey Champ
+                </div>
+                <form>
+                    <div>
+                        <Input type={'text'} name={'username'} placeholder={'Username'} value={username} setValue={setUsername} />
+                        <Input type={'email'} name={'email'} placeholder={'Email'} value={email} setValue={setEmail} />
+                        <Input type={'password'} name={'password'} placeholder={'Password'} value={password} setValue={setPassword} />
+                        <Submit value={'Sign up'} />
+                        <span>Back</span>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
 }
