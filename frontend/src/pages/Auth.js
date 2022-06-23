46632d4486f3cd897da4ac5f2fdc35b69ececfe4
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 22:51:00 +0300

    Added a login function

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default function Auth() {
-    return (
-        <div>auth</div>
-    )
+export default function Auth(props) {
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        try {
+            let res = await axios({
+                url: "http://127.0.0.1:8000/api/login",
+                method: "POST",
+                data: {
+                    "email": email,
+                    "password": password
+                },
+            })
+            console.log(res)
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+        return (
+            <>
+
+            </>
+        )
+    }
 }
