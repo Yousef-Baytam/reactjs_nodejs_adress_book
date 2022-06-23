778d1c31f3256e28805f23341566b76e9cff2289
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 19:17:58 +0300

    Added a function to update a contact

--- Backend/Controllers/contactsController.js
+++ Backend/Controllers/contactsController.js
@@ -11,3 +11,9 @@ module.exports.addContacts = async (req, res) => {
     const result = await contacts.save()
     res.send({ "success": true, "results": result })
 }
+
+module.exports.updateContacts = async (req, res) => {
+    const { fullName, phone, email, relationshipStatus, adress } = req.body
+    const contacts = await Contact.findByIdAndUpdate(req.params.id, { fullName, phone, email, relationshipStatus, adress }, { new: true, runValidators: true })
+    res.send({ "success": true, "results": contacts })
+}
