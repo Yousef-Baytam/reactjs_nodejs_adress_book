bc292dd8f7ec1552b3fb639e35232be238049d8e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 13:09:18 +0300

    Added a login route and a function

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -5,7 +5,7 @@ const auth = require('../Controllers/authController')
 
 router.get('/me',)
 router.post('/register', auth.register)
-router.post('/login',)
+router.post('/login', passport.authenticate('local'), auth.login)
 router.post('/logout',)
 
 module.exports = router
