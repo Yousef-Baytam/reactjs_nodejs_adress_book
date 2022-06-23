d1f99db0771321f0a71c8a32ff522d1160f5c7f6
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 22:52:13 +0300

    Added a cubmit button component

---
+++ frontend/src/components/Submit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Submit(props) {
+    return (
+        <div>
+            <input className='submit' type={'submit'} value={props.value} onClick={(e) => { e.preventDefault(); props.run() }} />
+        </div>
+    )
+}
