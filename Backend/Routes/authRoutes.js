336e752a539caacc214bbbd77983d139f0b50eb5
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 13:06:22 +0300

    Added a register route and function

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
+const auth = require('../Controllers/authController')
 
 router.get('/me',)
-router.post('/register',)
+router.post('/register', auth.register)
 router.post('/login',)
 router.post('/logout',)
 
