a8c43203c09c24e05fc20d1e60c21444214226df
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 22:43:16 +0300

    Added a function to get user info if authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import Header from './pages/Header';
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import Header from './pages/Header'
 import Auth from './pages/Auth'
-import './App.css';
-
-
+import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({})
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [contacts, setContacts] = useState([])
+
+  const handleAuthentication = async () => {
+    try {
+      let res = await axios({
+        url: `http://127.0.0.1:777/me`,
+        method: "Get",
+        // headers: {
+        //   Cookie: `bearer ${ localStorage.getItem('token') }`
+        // },
+      })
+      console.log(res)
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    handleAuthentication()
+  }, [])
+
   return (
     <>
       <Header />
