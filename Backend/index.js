52155e37e81a8cae29f19906b93518c807a9e66d
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 11:06:55 +0300

    Initialized some of the project dependecies

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,24 @@ mongoose.connect('mongodb://127.0.0.1:27017/AddressBook')
         console.log('ERROR!!', err)
     })
 
+app.use(session({
+    secret: 'my cat likes to jumpscare me',
+    name: 'session',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/CarpoolingApp' }),
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
+app.use(cors())
+app.use(express.json())
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.listen('777', () => {
     console.log('Listening for requests on port 77777777777777777777777777777777')
 })
