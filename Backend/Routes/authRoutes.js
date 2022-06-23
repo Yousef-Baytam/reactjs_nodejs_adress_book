74389cfc13be72073fa303e414343a6640e27b1f
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 13:10:07 +0300

    Added a me route and a function

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 const router = express.Router()
 const auth = require('../Controllers/authController')
 
-router.get('/me',)
+router.get('/me', auth.me)
 router.post('/register', auth.register)
 router.post('/login', passport.authenticate('local'), auth.login)
 router.post('/logout', auth.logout)
