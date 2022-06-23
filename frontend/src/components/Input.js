d8d2b1393f8cfa9c0b236afb5153c90eb786d31b
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-23 22:51:44 +0300

    Added an input component

---
+++ frontend/src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Input(props) {
+    return (
+        <div>
+            <label htmlFor={`${ props.name }`}>{props.placeholder}</label><br />
+            <input type={props.type} name={props.name} id={`${ props.name }`} placeholder={props.placeholder} value={props.value} onChange={(e) => {
+                props.setValue(e.target.value)
+            }} />
+        </div>
+    )
+}
