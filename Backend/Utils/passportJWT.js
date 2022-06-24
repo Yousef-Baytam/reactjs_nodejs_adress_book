81bdda770bd7dff1b73fa2c2eafb6841eb8e1c93
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-24 19:47:18 +0300

    Added a new passport strategyto check the validity of jwt tokens

---
+++ Backend/Utils/passportJWT.js
@@ -0,0 +1,26 @@
+const User = require('../Models/user')
+const process = require('dotenv').config()
+jwt_key = process.parsed.token_secret
+
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwt_key
+}
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        User.findOne({ id: jwt_payload.id }, function (err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+    }))
+}
