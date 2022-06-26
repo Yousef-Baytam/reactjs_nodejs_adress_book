2f19a953799a7ab69ca48e6f6f21854c3c10f3f2
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-26 14:44:04 +0300

    Added animation to the auth forms

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -10,6 +10,13 @@ export default function Auth(props) {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [loginDnone, setLoginDnone] = useState(false)
+    const [registerDnone, setRegisterDnone] = useState(true)
+    const [fadeIn, setFadeIn] = useState(false)
+    const [fadeOut, setFadeOut] = useState(false)
+    const [slideIn, setSlideIn] = useState(false)
+    const [slideOut, setSlideOut] = useState(false)
+    const [backNforth, setbackNforth] = useState(true)
 
     const handleLogin = async () => {
         try {
@@ -58,21 +65,35 @@ export default function Auth(props) {
                 <div>
                     My Phone Book
                 </div>
-                <form className=''>
+                <form className={`${ fadeIn ? 'fade-out' : '' } ${ fadeOut ? 'fade-in' : '' } ${ loginDnone ? 'translate' : '' }`}
+                    onAnimationEnd={() => {
+                        if (backNforth) {
+                            setLoginDnone(true); setRegisterDnone(false); setFadeIn(false); setSlideIn(true); setFadeOut(false); setSlideOut(false);
+                        } else {
+                            setLoginDnone(false); setRegisterDnone(true); setFadeIn(false); setSlideIn(false); setFadeOut(false); setSlideOut(false);
+                        }
+                    }}>
                     <div className='form-content'>
                         <Input type={'text'} name={'usernameLogin'} placeholder={'Username'} value={username} setValue={setUsername} />
                         <Input type={'password'} name={'passwordLogin'} placeholder={'Password'} value={password} setValue={setPassword} />
                         <Submit value={'Login'} run={handleLogin} />
-                        <div className='form-footer'>Dont have an accout? <span className='form-action'>Register</span></div>
+                        <div className='form-footer'>Dont have an accout? <span className='form-action' onClick={() => { setFadeIn(true); setFadeOut(false); setSlideOut(false); setbackNforth(true) }}>Register</span></div>
                     </div>
                 </form>
-                <form className=''>
+                <form className={`${ slideIn ? 'slide-in' : '' } ${ slideOut ? 'slide-out' : '' } ${ registerDnone ? 'translate' : '' }`}
+                    onAnimationEnd={() => {
+                        if (backNforth) {
+                            setLoginDnone(true); setRegisterDnone(false); setFadeIn(false); setSlideIn(false); setFadeOut(false); setSlideOut(false);
+                        } else {
+                            setLoginDnone(false); setRegisterDnone(true); setFadeIn(false); setSlideIn(false); setFadeOut(true); setSlideOut(false);
+                        }
+                    }}>
                     <div>
                         <Input type={'text'} name={'usernameRegister'} placeholder={'Username'} value={username} setValue={setUsername} />
                         <Input type={'email'} name={'emailRegister'} placeholder={'Email'} value={email} setValue={setEmail} />
                         <Input type={'password'} name={'passwordRegister'} placeholder={'Password'} value={password} setValue={setPassword} />
                         <Submit value={'Sign up'} run={handleRegister} />
-                        <span className='form-footer form-action'>Back</span>
+                        <span className='form-footer form-action' onClick={() => { setFadeIn(false); setSlideIn(false); setSlideOut(true); setbackNforth(false) }}>Back</span>
                     </div>
                 </form>
             </div>
